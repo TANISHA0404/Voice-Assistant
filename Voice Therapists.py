1125b2c93bdcc4c61a69ba758d80f8913a2a3910
TANISHA0404 <138234150+TANISHA0404@users.noreply.github.com>
2025-11-01 21:32:28 +0530

    Create Voice Therapists.py

---
+++ Voice Therapists.py
@@ -0,0 +1,389 @@
+import subprocess
+import wolframalpha
+import pyttsx3
+import tkinter as tk
+from tkinter import scrolledtext
+import json
+import random
+import operator
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import winshell
+import pyjokes
+import feedparser
+import smtplib
+import ctypes
+import time
+import requests
+import shutil
+from twilio.rest import Client
+from clint.textui import progress
+from ecapture import ecapture as ec
+from bs4 import BeautifulSoup
+import win32com.client as wincl
+from urllib.request import urlopen
+import openai
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+	engine.say(audio)
+	engine.runAndWait()
+
+def wishMe():
+	hour = int(datetime.datetime.now().hour)
+	if hour>= 0 and hour<12:
+		speak("Good Morning !")
+
+	elif hour>= 12 and hour<18:
+		speak("Good Afternoon !")
+
+	else:
+		speak("Good Evening !")
+
+	assname =("Therapist 1 point o")
+	speak("I am your Assistant")
+	speak(assname)
+	
+
+def username():
+	speak("What should i call you")
+	uname = takeCommand()
+	speak("Welcome")
+	speak(uname)
+	columns = shutil.get_terminal_size().columns
+	
+	print("#####################".center(columns))
+	print("Welcome", uname.center(columns))
+	print("#####################".center(columns))
+	
+	speak("How can i Help you")
+
+def takeCommand():
+	
+	r = sr.Recognizer()
+	
+	with sr.Microphone() as source:
+		
+		print("Listening...")
+		r.pause_threshold = 1
+		audio = r.listen(source)
+
+	try:
+		print("Recognizing...")
+		query = r.recognize_google(audio, language ='en-in')
+		print(f"User said: {query}\n")
+
+	except Exception as e:
+		print(e)
+		print("Unable to Recognize your voice.")
+		return "None"
+	
+	return query
+
+prompt_list = ['You are a therapist and will answer as a therapist',
+               '\nHuman: What time is it?',
+               '\nAI: I have no idea, I\'m a therapist!']
+
+openai.api_key = ""
+
+def update_list(message: str, pl: list[str]):
+    pl.append(message)
+
+def create_prompt(message: str, pl: list[str]) -> str:
+    p_message: str = f'\nHuman: {message}'
+    update_list(p_message, pl)
+    prompt: str = ''.join(pl)
+    return prompt
+
+def get_api_response(prompt: str) -> str | None:
+    text: str | None = None
+
+    try:
+        response: dict = openai.Completion.create(
+            model='text-davinci-003',  # Update the model if necessary
+            prompt=prompt,
+            temperature=0.9,
+            max_tokens=150,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6,
+            stop=[' Human:', ' AI:']
+        )
+
+        choices: dict = response.get('choices')[0]
+        text = choices.get('text')
+
+    except Exception as e:
+        print('ERROR:', e)
+
+    return text
+
+def get_bot_response(message: str, pl: list[str]) -> str:
+    prompt: str = create_prompt(message, pl)
+    bot_response: str = get_api_response(prompt)
+
+    if bot_response:
+        update_list(bot_response, pl)
+        pos: int = bot_response.find('\nAI: ')
+        bot_response = bot_response[pos + 5:]
+    else:
+        bot_response = 'Something went wrong...'
+
+    return bot_response
+
+
+
+if __name__ == '__main__':
+	clear = lambda: os.system('cls')
+	
+	# This Function will clean any command before execution of this python file
+	clear()
+	wishMe()
+	username()
+	
+	while True:
+		
+		query = takeCommand().lower()
+		
+		# All the commands said by user will be
+		# stored here in 'query' and will be
+		# converted to lower case for easily
+		# recognition of command
+		if 'wikipedia' in query:
+			speak('Searching Wikipedia...')
+			query = query.replace("wikipedia", "")
+			results = wikipedia.summary(query, sentences = 3)
+			speak("According to Wikipedia")
+			print(results)
+			speak(results)
+
+		elif 'open youtube' in query:
+			speak("Here you go to Youtube\n")
+			webbrowser.open("youtube.com")
+
+		elif 'open google' in query:
+			speak("Here you go to Google\n")
+			webbrowser.open("google.com")
+
+		elif 'play music' in query or "play song" in query:
+			speak("Here you go with music")
+			# music_dir = "G:\\Song"
+			music_dir = "C:\\Users\\GAURAV\\Music"
+			songs = os.listdir(music_dir)
+			print(songs)
+			random = os.startfile(os.path.join(music_dir, songs[1]))
+
+		elif 'the time' in query:
+			strTime = datetime.datetime.now().strftime("% H:% M:% S")
+			speak(f"Sir, the time is {strTime}")
+
+		elif 'how are you' in query:
+			speak("I am fine, Thank you")
+			speak("How are you")
+
+		elif 'fine' in query or "good" in query:
+			speak("It's good to know that your fine")
+
+		elif "change my name to" in query:
+			query = query.replace("change my name to", "")
+			assname = query
+
+		elif "change name" in query:
+			speak("What would you like to call me ")
+			assname = takeCommand()
+			speak("Thanks for naming me")
+
+		elif "what's your name" in query or "What is your name" in query:
+			speak("My friends call me")
+			speak(assname)
+			print("My friends call me", assname)
+
+		elif 'exit' in query:
+			speak("Thanks for giving me your time")
+			exit()
+
+		elif "who made you" in query or "who created you" in query:
+			speak("I have been created by Anandita, Tanisha and Rishima.")
+			
+		elif 'joke' in query:
+			speak(pyjokes.get_joke())
+			
+		elif "calculate" in query:
+			
+			app_id = "Wolframalpha api id"
+			client = wolframalpha.Client(app_id)
+			indx = query.lower().split().index('calculate')
+			query = query.split()[indx + 1:]
+			res = client.query(' '.join(query))
+			answer = next(res.results).text
+			print("The answer is " + answer)
+			speak("The answer is " + answer)
+
+		elif 'search' in query or 'play' in query:
+			
+			query = query.replace("search", "")
+			query = query.replace("play", "")		
+			webbrowser.open(query)
+
+		elif "who i am" in query:
+			speak("If you talk then definitely your human.")
+
+		elif "why you came to world" in query:
+			speak("Thanks to Anandita, Tanisha and Rishima. They made me.")
+
+		elif 'is love' in query:
+			speak("It is 7th sense that destroy all other senses")
+
+		elif "who are you" in query:
+			speak("I am your virtual assistant created by Gaurav")
+
+		elif 'news' in query:
+			
+			try:
+				jsonObj = urlopen('''https://newsapi.org / v1 / articles?source = the-times-of-india&sortBy = top&apiKey =\\times of India Api key\\''')
+				data = json.load(jsonObj)
+				i = 1
+				
+				speak('here are some top news from the times of india')
+				print('''=============== TIMES OF INDIA ============'''+ '\n')
+				
+				for item in data['articles']:
+					
+					print(str(i) + '. ' + item['title'] + '\n')
+					print(item['description'] + '\n')
+					speak(str(i) + '. ' + item['title'] + '\n')
+					i += 1
+			except Exception as e:
+				
+				print(str(e))
+
+		elif 'shutdown system' in query:
+				speak("Hold On a Sec ! Your system is on its way to shut down")
+				subprocess.call('shutdown / p /f')
+
+		elif "don't listen" in query or "stop listening" in query:
+			speak("for how much time you want to stop jarvis from listening commands")
+			a = int(takeCommand())
+			time.sleep(a)
+			print(a)
+
+		elif "where is" in query:
+			query = query.replace("where is", "")
+			location = query
+			speak("User asked to Locate")
+			speak(location)
+			webbrowser.open("https://www.google.nl / maps / place/" + location + "")
+
+		elif "camera" in query or "take a photo" in query:
+			ec.capture(0, "Jarvis Camera ", "img.jpg")
+
+		elif "restart" in query:
+			subprocess.call(["shutdown", "/r"])
+			
+		elif "hibernate" in query or "sleep" in query:
+			speak("Hibernating")
+			subprocess.call("shutdown / h")
+
+		elif "write a note" in query:
+			speak("What should i write, sir")
+			note = takeCommand()
+			file = open('jarvis.txt', 'w')
+			speak("Sir, Should i include date and time")
+			snfm = takeCommand()
+			if 'yes' in snfm or 'sure' in snfm:
+				strTime = datetime.datetime.now().strftime("% H:% M:% S")
+				file.write(strTime)
+				file.write(" :- ")
+				file.write(note)
+			else:
+				file.write(note)
+		
+		elif "show note" in query:
+			speak("Showing Notes")
+			file = open("jarvis.txt", "r")
+			print(file.read())
+			speak(file.read(6))
+					
+		# NPPR9-FWDCX-D2C8J-H872K-2YT43
+		elif "jarvis" in query:
+			
+			wishMe()
+			speak("Jarvis 1 point o at your service")
+			speak(assname)
+
+		elif "weather" in query:
+			
+			# Google Open weather website
+			# to get API of Open weather
+			api_key = "Api key"
+			base_url = "http://api.openweathermap.org / data / 2.5 / weather?"
+			speak(" City name ")
+			print("City name : ")
+			city_name = takeCommand()
+			complete_url = base_url + "appid =" + api_key + "&q =" + city_name
+			response = requests.get(complete_url)
+			x = response.json()
+			
+			if x["code"] != "404":
+				y = x["main"]
+				current_temperature = y["temp"]
+				current_pressure = y["pressure"]
+				current_humidiy = y["humidity"]
+				z = x["weather"]
+				weather_description = z[0]["description"]
+				print(" Temperature (in kelvin unit) = " +str(current_temperature)+"\n atmospheric pressure (in hPa unit) ="+str(current_pressure) +"\n humidity (in percentage) = " +str(current_humidiy) +"\n description = " +str(weather_description))
+			
+			else:
+				speak(" City Not Found ")
+
+		elif "wikipedia" in query:
+			webbrowser.open("wikipedia.com")
+
+		elif "Good Morning" in query:
+			speak("A warm" +query)
+			speak("How are you")
+			speak(assname)
+
+		# most asked question from google Assistant
+		elif "will you be my gf" in query or "will you be my bf" in query:
+			speak("I'm not sure about, may be you should give me some time")
+
+		elif "how are you" in query:
+			speak("I'm fine, glad you me that")
+
+		elif "i love you" in query:
+			speak("It's hard to understand")
+
+		elif "what is" in query or "who is" in query:
+			
+			# Use the same API key
+			# that we have generated earlier
+			client = wolframalpha.Client("API_ID")
+			res = client.query(query)
+			
+			try:
+				print (next(res.results).text)
+				speak (next(res.results).text)
+			except StopIteration:
+				print ("No results")
+
+		elif "therapist" in query:
+			print("What do you want to talk about?")
+			speak("Sure, what do you want to talk about")
+			user_input = takeCommand()
+			if user_input:
+				response = get_bot_response(user_input, prompt_list)
+				print('You: {user_input}\n')
+				print('Therapist: {response}\n')
+				speak(response)
+			else:
+				speak("Happy to help")
+				print("Happy to help!!")
